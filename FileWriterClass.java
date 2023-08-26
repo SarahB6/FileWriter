7f81c52a2dce119bfe1b8b1357a49cd62bb47db6
Sarah <forsugar88@gmail.com>
2023-08-26 07:30:14 -0700

    Added fileWriter

--- FileWriterClass.java
+++ FileWriterClass.java
@@ -1,17 +1,25 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayDeque;
 
 public class FileWriterClass
 {
     //take in a string and output a file
-    public void writeFile(String textEntry)
+    public void writeFile(String fileName, String textEntry) throws IOException
     {
-        
+        BufferedWriter bw = new BufferedWriter(new FileWriter( fileName));
+        for(int i = 0; i<textEntry.length(); i++)
+        {
+            bw.write(fileName.charAt(i));
+        }
+        bw.close();
+
     }
     //take in a file and output a string
-    public String readFile(String inputString, String fileName) throws IOException
+    public String readFile(String fileName) throws IOException
     {
 		BufferedReader br = new BufferedReader(new BufferedReader(new FileReader(fileName)));
         StringBuilder toReturn = new StringBuilder(); 
@@ -19,7 +27,8 @@ public class FileWriterClass
 		{
             toReturn.append(br.read());
         }
+        br.close();
         return toReturn.toString();
-        //hello
+
     }
 }
